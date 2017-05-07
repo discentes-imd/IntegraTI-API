d8b05bf1acf09fce697af83d9336b40b7f93e7e0
RodrigoNdeC <rodrigondec@gmail.com>
2017-05-07 17:26:14 -0300

    rota com form de POST

--- hello_flask.py
+++ hello_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -19,6 +19,20 @@ def hello_user(name):
 		return redirect(url_for('hello_admin'))
 	else:
 		return redirect(url_for('hello_guest',guest = name))
+  
+@app.route('/success/<name>')
+def success(name):
+	return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+	if request.method == 'POST':
+		user = request.form['nm']
+		return redirect(url_for('success',name = user))
+	else:
+		user = request.args.get('nm')
+		return redirect(url_for('success',name = user))
+
 if __name__ == "__main__":
 	app.debbug = True
 	app.run()
