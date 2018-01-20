c53a77822b28c79856a47cb42d8c7c177404c4c5
RodrigoNdeC <rodrigondec@gmail.com>
2018-01-20 12:36:45 -0300

    remove links

--- app/routes_need_login_config.py
+++ app/routes_need_login_config.py
@@ -8,8 +8,8 @@ routes = [
     ('/auth/user/<int:id>', ('GET', 'PUT', 'DELETE')),
     ('/auth/user/', ('GET')),
     ('/auth/login/', ('DELETE')),
-    ('/event/', ('POST', 'GET')),
-    ('/event/<int:id>', ('GET', 'PUT', 'DELETE')),
+    ('/event/', ('POST')),
+    ('/event/<int:id>', ('PUT', 'DELETE')),
     ('/event/type/', ('POST', 'GET')),
     ('/event/type/<int:id>', ('GET', 'PUT', 'DELETE')),
     ('/auth/user/resetpassword/', ('PUT'))
