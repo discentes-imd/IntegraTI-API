ef5404b73e1209e54bc3dd747b0eb3bd3acd8bd1
RodrigoNdeC <rodrigondec@gmail.com>
2018-02-04 20:10:07 -0300

    configs do db do heroku

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Statement for enabling the development environment
 # Enabling the development environment
 DEBUG = True
-DB_USER = 'rodrigondec'
-DB_PASS = ''
-DB_HOST = 'localhost'
-#DB_PORT = '80'
-DB_NAME = 'IntegraTI'
+DB_USER = 'zuknshrpyxbdjg'
+DB_PASS = 'b1651a9c9cd0adcb24f9dc4414f5f635f37ddaddebb8ca46dc86bd3d0008ecbc'
+DB_HOST = 'ec2-23-21-162-90.compute-1.amazonaws.com'
+DB_PORT = '5432'
+DB_NAME = 'd9jegcm8m9atr5'
 
 # Define the application directory
 import os
@@ -14,7 +14,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # Define the database - we are working with
 # SQLite for this example
 # SQLALCHEMY_DATABASE_URI = 'mysql://'+DB_USER+':'+DB_PASS+'@'+DB_HOST+'/'+DB_NAME
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=DB_USER,pw=DB_PASS,url=DB_HOST,db=DB_NAME)
+SQLALCHEMY_DATABASE_URI = 'postgres://{user}:{pw}@{url}:{port}/{db}'.format(user=DB_USER,pw=DB_PASS,url=DB_HOST,db=DB_NAME, port=DB_PORT)
 DATABASE_CONNECT_OPTIONS = {}
 
 # Application threads. A common general assumption is
