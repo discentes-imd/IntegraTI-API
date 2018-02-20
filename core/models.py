1dcae7f7622c7776ee340d1464a3c92ed581bfe0
RodrigoNdeC <rodrigondec@gmail.com>
2018-02-19 23:37:36 -0300

    Pasta adicionada

--- core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-
