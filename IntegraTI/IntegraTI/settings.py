5749f1a85e5eecd6ef7e86bf569c4547e7c43eff
RodrigoNdeC <rodrigondec@gmail.com>
2018-02-22 02:00:53 -0300

    whitenoise adicionado

--- IntegraTI/IntegraTI/settings.py
+++ IntegraTI/IntegraTI/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'IntegraTI.urls'
@@ -137,3 +138,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
